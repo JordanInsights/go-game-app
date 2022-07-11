d2bf28671c18aa4ab8fc15fcb81ae400adb86527
Jordan Shaw <jordan.m.shaw@bjss.com>
2022-07-11 13:14:03 +0100

    Add initial stages of server

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	handler := http.HandlerFunc(PlayerServer)
+	log.Fatal(http.ListenAndServe(":5000", handler))
 }
